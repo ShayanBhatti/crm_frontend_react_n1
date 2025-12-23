54be6a6945b5a01297c53f5ed1c94ee34676797b
Shayan Bhatti <shayanbhatti332@gmail.com>
2025-12-23 19:17:32 +0500

    routes_created

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter ,Navigate} from "react-router-dom";
 import AuthLayout from "../components/layout/AuthLayout";
 import DashboardLayout from "../components/layout/DashboardLayout";
 import Login from "../features/auth/Login";
@@ -8,6 +8,10 @@ export const router = createBrowserRouter([
   {
     element: <AuthLayout />,
     children: [
+       {
+        path: "/",
+        element: <Navigate to="/login" replace />, // redirect root to login
+      },
       {
         path: "/login",
         element: <Login />,
