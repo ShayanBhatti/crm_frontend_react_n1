98ab078daa9dbebb422c95d406e19c0539209f86
Shayan Bhatti <shayanbhatti332@gmail.com>
2025-12-23 17:58:09 +0500

    tailwind_setup_complted

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 function App() {
   return (
-    <div>
-      CRM Frontend
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <h1 className="text-3xl font-bold text-indigo-600">
+        Tailwind + Vite Working
+      </h1>
     </div>
   );
 }
+
 export default App;
